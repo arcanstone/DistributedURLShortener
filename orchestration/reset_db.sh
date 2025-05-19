a8ebc1ae20c248e7d09bd1ce21c200643ee63552
Allan <allan.henriques@mail.utoronto.ca>
2025-05-19 18:52:19 -0400

    Update reset_db.sh

--- orchestration/reset_db.sh
+++ orchestration/reset_db.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # List of server IP addresses
-servers=("dh2010pc29" "dh2010pc30" "dh2010pc31" "dh2020pc04")
+servers=("" "" "" "")
 
 # Path to the serverSqlite directory on the remote servers
-script_dir="/student/henriq93/Documents/409assign/a1group92/a1/serverSqlite"
+script_dir=
 
 # Loop through each server and SSH to run the runit.bash script
 for server in "${servers[@]}"; do
